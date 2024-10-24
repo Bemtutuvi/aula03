a8b2e319d3aa1a18639ef2727ad2164cc91e1f04
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-10-24 10:02:10 -0400

    Finalizado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from "react";
+import ListaProdutos from "../components/ListaProdutos";
 
 export default function Home(){
 
-const [produto,setProdutos] = useState([]);
+const [produtos, setProdutos] = useState([]);
 
 useEffect(()=>{
     const receberListaProdutos= async()=>{
@@ -11,25 +12,15 @@ useEffect(()=>{
         const dados = await resposta.json();
         setProdutos(dados);
         } catch(erro){
-            //falta um console.error()
+            console.error(erro)
         }
-        receberListaProdutos();
     }
     receberListaProdutos();
 },[]);
 
 return(
     <>
-    <h1>Lista de Produtos</h1>
-    <ul>
-        {produtos.map(produto=>(
-            <li key={produto.id}>
-                <h2>{produto.title}</h2>
-                <p>{produto.descreption}</p>
-                <p>Preço: R${produto.price}</p>
-                <img src={produto.image} alt={produto.title} width={100}/>
-            </li>))}
-            </ul>
+    <ListaProdutos produtos={produtos}/>
             </>
 );
 }
