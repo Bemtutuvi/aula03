9b2863b21317fe37f14b14be53121524a125486f
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-10-28 14:06:55 -0400

    começo do css

--- src/components/ListaProdutos.jsx
+++ src/components/ListaProdutos.jsx
@@ -1,15 +1,18 @@
+import styles from '../styles/listaProdutos.module.css'
 export default function ListaProdutos({ produtos }){
     return(
         <>
     <h1>Lista de Produtos</h1>
-    <ul>
+    <ul className={styles.blocoLista}>
+        <div className={styles.card}>
         {produtos.map(produto=>(
             <li key={produto.id}>
-                <h2>{produto.title}</h2>
-                <p>{produto.descreption}</p>
-                <p>Preço: R${produto.price}</p>
+                <h2>{produto.title}</h2>                
                 <img src={produto.image} alt={produto.title} width={100}/>
+                <p>Preço: R${produto.price}</p>
+                <p>{produto.descreption}</p>
             </li>))}
+            </div>
             </ul>
                 </>
         )
