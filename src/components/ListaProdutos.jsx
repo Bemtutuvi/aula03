077f57a4c89643a2025cd298894253c6194d7a53
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-11-03 10:34:46 -0400

    começo do css

--- src/components/ListaProdutos.jsx
+++ src/components/ListaProdutos.jsx
@@ -2,13 +2,23 @@ import styles from '../styles/listaProdutos.module.css'
 export default function ListaProdutos({ produtos }){
     return(
         <>
+        <header>
+            <h1>Roupas Marcelindo - Lindo</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Blog</li>
+                </ul>
+            </nav>
+        </header>
     <h1>Lista de Produtos</h1>
     <ul className={styles.blocoLista}>
         {produtos.map(produto=>(
         <div key={produto.id} className={styles.card}>
                 <h2>{produto.title}</h2>                
                 <img src={produto.image} alt={produto.title} width={100}/>
-                <p>Preço: R${produto.price}</p>
+                <p class="preco">Preço: R${produto.price}</p>
                 <p>{produto.descreption}</p>
             </div>))}
             </ul>
