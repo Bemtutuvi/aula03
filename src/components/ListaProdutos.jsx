483a199048659f4db24b7e68a00ad955e146a997
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-11-03 10:00:12 -0400

    arrumou o problema

--- src/components/ListaProdutos.jsx
+++ src/components/ListaProdutos.jsx
@@ -4,15 +4,13 @@ export default function ListaProdutos({ produtos }){
         <>
     <h1>Lista de Produtos</h1>
     <ul className={styles.blocoLista}>
-        <div className={styles.card}>
         {produtos.map(produto=>(
-            <li key={produto.id}>
+        <div key={produto.id} className={styles.card}>
                 <h2>{produto.title}</h2>                
                 <img src={produto.image} alt={produto.title} width={100}/>
                 <p>Preço: R${produto.price}</p>
                 <p>{produto.descreption}</p>
-            </li>))}
-            </div>
+            </div>))}
             </ul>
                 </>
         )
