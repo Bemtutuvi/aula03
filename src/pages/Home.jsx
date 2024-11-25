247c4bb15715beca1f2160a18d2b6aad1163ee04
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-11-25 13:06:26 -0400

    ordenar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import ListaProdutos from "../components/ListaProdutos";
 export default function App(){
+    
 const [produtos, setProdutos] = useState([]);
 
 useEffect(()=>{
@@ -9,24 +10,39 @@ useEffect(()=>{
             const resposta= await fetch('https://fakestoreapi.com/products');
         const dados = await resposta.json();
         setProdutos(dados);
-        } catch(erro){
-            console.error(erro)
+        } catch(error){
+            alert("A conexão do API está incorreta");
         }
-    }
+        }
     receberListaProdutos();
 },[]);
-if(ListaProdutos.lenght===0){
+if(produtos.lenght===0){
     return<h1>Carregando...</h1>
 }
 
 const orderAZ= ()=>{
-    const ListaAux=[...Lista].filter((a,b)=> a.title.localeCompare(b.title));
-    setLista(ListaAux);
+    const ListaAux=[...produtos].sort((a,b)=> a.title.localeCompare(b.title));
+    setProdutos(ListaAux);
+}
+const orderZA= ()=>{
+    const ListaAux=[...produtos].sort((a,b)=> b.title.localeCompare(a.title));
+    setProdutos(ListaAux);
+}
+const orderPrecoMaior= ()=>{
+    const ListaAux=[...produtos].sort((a,b)=> a.price.localeCompare(b.price));
+    setProdutos(ListaAux);
+}
+const orderPrecoMenor= ()=>{
+    const ListaAux=[...produtos].sort((a,b)=> b.price.localeCompare(a.price));
+    setProdutos(ListaAux);
 }
 
 return(
     <>
-    <button onClick={()=>orderAZ}>AZ</button>
+    <button onClick={()=>orderAZ}>A à Z</button>
+    <button onClick={()=>orderZA}>Z à A</button>
+    <button onClick={()=>orderPrecoMaior}>Maior Preço</button>
+    <button onClick={()=>orderPrecoMenor}>Menor Preço</button>
     <ListaProdutos produtos={produtos}/>
             </>
 );
